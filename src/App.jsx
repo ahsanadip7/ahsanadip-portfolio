2613735a45dbd88eefabdc6d10f0551de4e046bf
ahsanadip7 <ahsanadip7@gmail.com>
2025-05-12 03:40:14 +0600

    polished

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,13 @@ function App() {
 
 
   return (
-    <>
-     
-     <NavBar />
+     <>
+    <NavBar />
     <section id="home">
       <HeroSection />
     </section>
-    <section id="social-links">
-      <SocialLinks />
-    </section>
+    <div className='bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a]'>
+     
     <section id="about">
       <AboutMe />
     </section>
@@ -29,14 +27,16 @@ function App() {
       <Skills />
     </section>
     <section id="projects">
-      <ProjectSection />
+      <ProjectSection></ProjectSection>
     </section>
     <section id="contact">
-      <ContactSection />
+      <SocialLinks></SocialLinks>
     </section>
+    
+
+    </div>
     <Footer />
-      
-    </>
+  </>
   )
 }
 
